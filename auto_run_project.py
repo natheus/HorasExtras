da9a369025bb7d9ddae00d12bc63b0ea4f050881
Matheus Nunes <matheus.nunes@villelabrasillabs.com>
2023-08-07 21:45:17 -0300

    configs script

--- auto_run_project.py
+++ auto_run_project.py
@@ -6,8 +6,18 @@ caminho_backend = os.path.abspath("backend/HorasExtras")
 caminho_frontend = os.path.abspath("frontend/HorasExtras")
 
 # Comandos para executar os projetos
-comando_backend = 'dotnet run'  # Comando de execução do projeto C#
-comando_frontend = 'npm start'  # Comando de execução do projeto Angular
+comando_frontend = 'npm start'  # Comando de execução do projeto Frontend
+comando_backend = "dotnet run"
+
+# Comandos para instalar as dependências
+comando_instalar_dependencias_back = "dotnet restore"
+comando_instalar_dependencias_front = "npm install"
+
+# Instalar as dependências do projeto backend
+subprocess.Popen(comando_instalar_dependencias_back, cwd=caminho_backend, shell=True)
+
+# Instalar as dependências do projeto frontend
+subprocess.Popen(comando_instalar_dependencias_front, cwd=caminho_frontend, shell=True)
 
 # Executar o projeto backend
 subprocess.Popen(comando_backend, cwd=caminho_backend, shell=True)
